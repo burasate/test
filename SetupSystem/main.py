c1fa59cc2697f5bdf9ed2b4b62663407cad97f29
DEX3D <61697687+burasate@users.noreply.github.com>
2020-05-11 00:16:09 +0700

    Update main.py

--- SetupSystem/main.py
+++ SetupSystem/main.py
@@ -1,1 +1,22 @@
 print('Online Script Loaded')
+
+from maya import cmds
+from maya import mel
+import urllib2
+import shutil
+import zipfile
+import os
+
+def formatPath(path):
+    path = path.replace("/", os.sep)
+    path = path.replace("\\", os.sep)
+    return path
+  
+mayaAppDir = mel.eval('getenv MAYA_APP_DIR')
+#scriptsDir = "%s%sscripts"%(mayaAppDir, os.sep)
+scriptsDir = mayaAppDir+os.sep+'scripts'
+userSetupFile = scriptsDir + os.sep + "userSetup.py" 
+
+print ('mayaAppDir = '+mayaAppDir)
+print ('scriptsDir = '+scriptsDir)
+print ('userSetupFile = '+userSetupFile)
