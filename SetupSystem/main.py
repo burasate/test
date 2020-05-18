1c8ae840adc217e178e1462a7b750bac38fc7343
DEX3D <61697687+burasate@users.noreply.github.com>
2020-05-19 00:57:59 +0700

    Update main.py

--- SetupSystem/main.py
+++ SetupSystem/main.py
@@ -1,1 +1,28 @@
 print('Online Script Loaded\n')
+
+"""
+https://raw.githubusercontent.com/burasate/test/master/SetupSystem/main.py
+TESTING ZONE
+"""
+from maya import cmds
+from maya import mel
+import urllib2
+import shutil
+import zipfile
+import os
+
+
+def formatPath(path):
+    path = path.replace("/", os.sep)
+    path = path.replace("\\", os.sep)
+    return path
+
+mayaAppDir = formatPath(mel.eval('getenv MAYA_APP_DIR'))
+scriptsDir = formatPath(mayaAppDir + os.sep + 'scripts')
+projectDir = formatPath(scriptsDir+os.sep+'BRSLocDelay')
+userFile = formatPath(projectDir + os.sep + "user")
+
+print ('mayaAppDir = ' + mayaAppDir)
+print ('scriptsDir = ' + scriptsDir)
+print ('projectDir = ' + projectDir)
+print ('userSetupFile = ' + userFile)
